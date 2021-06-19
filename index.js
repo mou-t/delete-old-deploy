95a1210ad646f36d0fbe544f522798c6a4320d23
mou-t <detective0218gm@gmail.com>
2021-06-19 19:21:50 +0900

    公式の古いデプロイを削除するコードをコピー

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+const endpoint = "https://api.cloudflare.com/client/v4/accounts/" + ACCOUNT_ID + "/pages/projects/" + PROJECT_NAME + "/deployments";
+const email = EMAIL;
+
+addEventListener("scheduled", (event) => {
+  event.waitUntil(handleScheduled(event.scheduledTime));
+});
+
+async function handleScheduled(request) {
+  const init = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      "X-Auth-Email": email,
+      "X-Auth-Key": API_KEY,
+      //We recommend you store API keys as secrets using the Workers dashboard or using Wrangler as documented here https://developers.cloudflare.com/workers/cli-wrangler/commands#secret
+    },
+  };
+
+  const response = await fetch(endpoint, init);
+  return new Response(200);
 }
